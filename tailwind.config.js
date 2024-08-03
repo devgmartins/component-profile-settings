064ff01568695236e0fa19477a289f5fd5dd7f10
Alison <gabrielalison341@gmail.com>
2024-08-02 22:38:29 -0300

    component profile settings

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  darkMode: "media",
+  theme: {
+    extend: {
+      fontFamily: {
+        heebo: ["Heebo", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
